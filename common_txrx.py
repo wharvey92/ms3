fcec714f62b3fd490aba4bc846b5c4963628b4c2
Will Harvey <wharvey@stanford.edu>
2014-06-01 21:48:53 -0700

    dot product

--- common_txrx.py
+++ common_txrx.py
@@ -5,8 +5,15 @@ import operator
 import binascii
 # Methods common to both the transmitter and receiver.
 
+def dot_vectors(vec1, vec2):
+	dot_sum = 0
+	if len(vec1) != len(vec2):
+		print "Error: Dot product of two different length vectors"
+		return -1
 
-
+	for i in xrange(len(vec1)):
+		dot_sum += vec1[i] * vec2[i]
+	return dot_sum
 
 def lpfilter(samples_in, omega_cut):
     '''
