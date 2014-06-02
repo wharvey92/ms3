d89420e289e77fa5d2917a388fcc8d9ac32fa443
Will Harvey <wharvey@stanford.edu>
2014-06-01 23:24:44 -0700

    lp filter, done with transmitter

---
+++ test_script.py
@@ -0,0 +1,15 @@
+from common_txrx import getlpfilter_vector, perform_convolution
+import numpy as np
+
+
+
+x = np.array([1, 2, 4, 5, 6, 7, 8])
+h = getlpfilter_vector(0.5)
+print x
+print h
+
+print 
+print 
+print
+
+print perform_convolution(x, h)
